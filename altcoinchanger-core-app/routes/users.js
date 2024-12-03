e0993fd461e5a63c080e078ab9583f973b9cc199
Gracious Mashasha <graccasoft263@gmail.com>
2024-12-03 13:59:11 +0200

    Update users.js

--- altcoinchanger-core-app/routes/users.js
+++ altcoinchanger-core-app/routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var request = require('request');
 var router = express.Router();
 var session = require('express-session')({
-  secret: 'l0tu5n0t35',
+  secret: '',
   resave: true,
   saveUninitialized: true,
   cookie: { secure: false }
